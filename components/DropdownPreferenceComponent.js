8d9131371615194b9eb5356da8d9c1b1e26cd406
Kod-Draken <m.dackhammar@gmail.com>
2025-04-24 16:25:24 +0200

    started basic styling to get visual references

--- components/DropdownPreferenceComponent.js
+++ components/DropdownPreferenceComponent.js
@@ -2,12 +2,18 @@ export default {
     props: ['label'],
     template:
         `
-    <label> 
-        {{ label }} 
-        <select name="" id="">
-            <option value="">options</option>
+    <div class="title-dropdown-container">
+        <label for="1"> 
+            {{ label }}  </label>
+        <select name="" class="dropdown-selector" id="1">
+            <option value="0" class="select">Välj...</option>
+            <option value="1">option 1</option>
+            <option value="2">option 2</option>
+            <option value="3">option 3</option>
+
         </select>
-    </label>
+    </div>
+
     <div class="selected">
         <div>selected options</div>
     </div>
