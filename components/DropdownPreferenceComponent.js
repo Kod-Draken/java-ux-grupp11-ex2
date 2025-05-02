1a325c886ee49f27dc9cc87343c6296cbc56d02e
Kod-Draken <m.dackhammar@gmail.com>
2025-05-02 12:45:11 +0200

    style basically finished for the preferences part of the application

--- components/DropdownPreferenceComponent.js
+++ components/DropdownPreferenceComponent.js
@@ -1,22 +1,25 @@
 export default {
     props: ['label'],
     template:
+            `
+        <div class="title-dropdown-container">
+            <label for="1"> 
+                {{ label }}  </label>
+            <select name="" class="dropdown-selector" id="1">
+                <option value="0" class="select">Välj...</option>
+                <option value="1">option 1</option>
+                <option value="2">option 2</option>
+                <option value="3">option 3</option>
+
+            </select>
+        </div>
+
+    <div>
+            <div class="selected-options">
+            <label for="remove-button">selected option</label>
+                <input class="remove-button"  type="image" src="assets/close.svg" alt="remove">
+            </div>
+        </div>
+
         `
-    <div class="title-dropdown-container">
-        <label for="1"> 
-            {{ label }}  </label>
-        <select name="" class="dropdown-selector" id="1">
-            <option value="0" class="select">Välj...</option>
-            <option value="1">option 1</option>
-            <option value="2">option 2</option>
-            <option value="3">option 3</option>
-
-        </select>
-    </div>
-
-    <div class="selected">
-        <div>selected options</div>
-    </div>
-
-    `
 }
