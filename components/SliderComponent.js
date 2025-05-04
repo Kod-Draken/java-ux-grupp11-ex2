7af9b66b6b499520e8a1738036ce5fba1c6ca94a
xzutv <haimenabbas@gmail.com>
2025-05-04 20:06:05 +0200

    added slider component

---
+++ components/SliderComponent.js
@@ -0,0 +1,18 @@
+export default {
+    mounted() {
+        // console.log('Dropdown options:', this.options);
+    },
+    // props: ['options'],
+    methods: {
+        // onSelect(event) {
+        //     this.$emit('selected', event.target.value)
+        // },
+    },
+    template:
+        `
+        <div>
+
+        </div>
+
+        `
+}
