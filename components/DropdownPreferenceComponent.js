e31c12cad640665758fd19dad556b84d99a0fd8d
Haimen <haimenabbas@gmail.com>
2025-05-05 14:03:21 +0200

    returned original select input instead of ul

--- components/DropdownPreferenceComponent.js
+++ components/DropdownPreferenceComponent.js
@@ -1,37 +1,23 @@
+
 export default {
-    data() {
-        return {
-            isOpen: false,
-            selected: null
-        }
-    },
     mounted() {
         console.log('Dropdown options:', this.options);
     },
     props: ['options'],
     methods: {
-        onSelect(item) {
-            this.selected = item;
-            this.isOpen = false;
-            this.$emit('select', item)
-            alert(item)
+        onSelect(event) {
+            this.$emit('select', event.target.value)
         },
-        toggleDropdown() {
-            this.isOpen = !this.isOpen;
-        }
     },
     template:
         `
-    <div class="dropdown">
-        <div class="dropdown-toggle" @click="toggleDropdown">
-            {{ 'Välj...' }}
-         </div>
-        <ul v-if="isOpen" class="dropdown-list">
-            <li class="dropdown-item" v-for="item in options" :key="item" @click="onSelect(item)">
+        <select @change="onSelect" class="dropdown-selector">
+            <option disabled selected>Välj...</option>
+            
+            <option v-for="item in options" :key="item" :value="item">
                 {{ item }} 
-            </li>
-        </ul>
-    </div>
+            </option>
+        </select>
 
         `
 }
