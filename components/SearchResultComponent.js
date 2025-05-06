93f5178d707122861f2cfbb31c826b2764e80734
Haimen <haimenabbas@gmail.com>
2025-05-06 09:15:19 +0200

    added next-arrow on receipe details

---
+++ components/SearchResultComponent.js
@@ -0,0 +1,84 @@
+
+
+export default {
+
+    template:
+        `
+                <div class="matching-recipes">
+                    <details class="recipe">
+                        <summary class="recipe-summary">
+                            <div class="section-title">
+                                Maträtt 1
+                            </div>
+                            <div id="summary-grid">
+                                <div>
+                                    <img src="assets/klocka.svg" alt="klocka">
+                                    Under 30 min
+                                    {{ time }}
+                                </div>
+                                <div>
+                                    <img src="assets/level.svg" alt="komplexitet 2">
+                                </div>
+                                <div>
+                                    {{ price }}
+                                    140
+                                    kr/p
+                                </div>
+                                <div>
+                                    {{ margin }}
+                                    30
+                                    % VM
+                                </div>
+                            </div>
+                            <div class="recipe-vector">
+                                <img src="assets/Vector-7.svg" alt="Pil ikon" class="arrow">
+                            </div>
+                        </summary>
+                        <div class="recipe-details">
+                            <div id="recipe-image">
+                                <img src="assets/unsplash_awj7sRviVXo.png" alt="">
+                            </div>
+                            <div class="column-groups">
+                                <div class="recipe-details-col-1">
+                                    <div>
+                                        <h5>Måltidstyp</h5>
+                                        <p>Huvudrätt</p>
+                                    </div>
+                                    <div>
+                                        <h5>Allergier</h5>
+                                        <p>Skaldjur</p>
+                                    </div>
+                                </div>
+                                <div class="recipe-details-col-2">
+                                    <div>
+                                        <h5>Tema</h5>
+                                        <p>Havet</p>
+                                    </div>
+                                    <div>
+                                        <h5>Kost</h5>
+                                        <p>Pesceterian</p>
+                                    </div>
+                                </div>
+                                <div class="recipe-details-col-3">
+                                    <div>
+                                        <h5>Kan frysas</h5>
+                                        <p>Ja</p>
+                                    </div>
+                                    <div>
+                                        <h5>Kan förlagas</h5>
+                                        <p>Ja</p>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <input class="view-recipe" type="image" src="assets/pil-nästa.svg" alt="remove">
+
+
+                        </div>
+                    </details>
+
+                </div>
+
+`
+}
